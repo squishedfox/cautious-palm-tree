814b5a14537de24a7705550b76fb51333596f32a
squishedfox <squishedfox@protonmail.com>
2026-06-03 12:55:39 -0500

    Update HistoryList.tsx

--- src/features/resume-builder/components/HistoryList.tsx
+++ src/features/resume-builder/components/HistoryList.tsx
@@ -10,7 +10,18 @@ const HistoryList = () => {
       <h1></h1>
       {jobHistory.items.map((item) => (
         <li key={item.companyName}>
-          <JobHistoryItem {...item} />
+          <div>
+            <div className="flex content-between">
+              <div>
+                <span>Up Arrow Chvron here</span>
+                <span>Down Arrow Chvron here</span>
+              </div>
+              <div>
+                <button>Trash Button Here</button>
+              </div>
+            </div>
+            <JobHistoryItem {...item} />
+          </div>
         </li>
       ))}
     </ul>
