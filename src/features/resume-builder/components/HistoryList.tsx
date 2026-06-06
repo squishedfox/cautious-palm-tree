c36262ae8adcecf6d3a203b5b4fe50c3dd1cde59
squishedfox <squishedfox@protonmail.com>
2026-06-05 21:41:11 -0500

    Adds loader for when deleting is happening

--- src/features/resume-builder/components/HistoryList.tsx
+++ src/features/resume-builder/components/HistoryList.tsx
@@ -13,14 +13,28 @@ const createEmptyJobHistoryItem = (): JobHistoryListItem => ({
 
 const HistoryList = () => {
   const [jobs, setJobs] = useState<object>({});
+  const [isDeleting, setIsDeleting] = useState(false);
 
   const addJobHandler = (event: MouseEvent<HTMLButtonElement>) => {
     event.preventDefault();
-    setJobs((prev) => ({
-      ...prev,
+    setJobs((prev) => Object.assign({
       [ulid()]: createEmptyJobHistoryItem(),
-    }));
+    }, prev));
   };
+
+  const deleteJobHandler = (event: MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+    setIsDeleting(true);
+    const [idToDelete]: Array<string> = event.currentTarget.id.split("-");
+
+    setJobs((prev) => Object.entries(prev).reduce((acc, [id, job]) => {
+      if (id !== idToDelete) {
+        acc[id] = job;
+      }
+      return acc;
+    }, {} as any));
+    setIsDeleting(false);
+  }
 
   return (
     <div>
@@ -36,7 +50,7 @@ const HistoryList = () => {
                 <ChevronIcon direction="down" />
               </div>
               <div>
-                <button><TrashIcon /></button>
+                <button disabled={isDeleting} aria-label="Delete job" id={`${id}-delete-btn`} onClick={deleteJobHandler}><TrashIcon /></button>
               </div>
             </div>
             <JobHistoryItem {...job} />
